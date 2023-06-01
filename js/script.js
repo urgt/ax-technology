9327dacda2bdd0debf599a83cfb48044b0b1e36c
G'ayrat Urinbaev <urinbaevg@gmail.com>
2023-06-01 12:31:51 +0500

    full home page

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 'auto',
+    spaceBetween: 32,
+    grabCursor: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 'auto',
+    spaceBetween: 12,
+    grabCursor: true
+});

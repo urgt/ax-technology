0028615fe1e0578995f9df65676749e35f8ce39d
G'ayrat Urinbaev <urinbaevg@gmail.com>
2023-06-06 12:58:10 +0500

    add cursor

--- js/script.js
+++ js/script.js
@@ -20,8 +20,106 @@ var swiper = new Swiper(".mySwiper", {
     }
 });
 
+
+$(() => {
+
+    $(() => {
+
+        $(window).mousemove(function (e) {
+
+            $('a').on('mouseenter', function () {
+
+                $('.cursor2').addClass('active');
+
+            });
+
+        });
+
+        $(window).mousemove(function (e) {
+
+            $('a').on('mouseleave', function () {
+
+                $('.cursor2').removeClass('active');
+
+            });
+
+        });
+    });
+
+    $(window).mousemove(function (e) {
+
+        $('a',).on('mouseenter', function () {
+
+            $('.cursor2').addClass('active');
+
+        });
+
+        $('.swiper-wrapper',).on('mouseenter', function () {
+
+            $('.cursor2').addClass('active').css({
+
+
+            });
+
+        });
+
+        $('.swiper-button-next').on('mouseenter', function () {
+
+            $('.cursor2').addClass('active');
+
+        });
+        $('.swiper-button-prev').on('mouseenter', function () {
+
+            $('.cursor2').addClass('active');
+
+        });
+
+    });
+
+    $(window).mousemove(function (e) {
+
+        $('a').on('mouseleave', function () {
+
+            $('.cursor2').removeClass('active');
+
+        });
+
+        $('.swiper-button-next').on('mouseleave', function () {
+
+            $('.cursor2').removeClass('active');
+
+        });
+
+        $('.swiper-button-prev').on('mouseleave', function () {
+
+            $('.cursor2').removeClass('active');
+
+        });
+
+        $('.swiper-wrapper',).on('mouseleave', function () {
+
+            $('.cursor2').removeClass('active');
+
+        });
+
+    });
+
+
+
+});
+
 var swiper = new Swiper(".mySwiper2", {
     slidesPerView: 'auto',
     spaceBetween: 12,
     grabCursor: true
 });
+
+
+document.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+        style: `
+		--move-x: ${e.clientX}px;
+		--move-y: ${e.clientY}px;
+		`
+    })
+})
